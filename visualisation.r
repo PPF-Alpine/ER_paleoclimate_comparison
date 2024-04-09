0aad914e8b232bb5766264c840c995240441beb5
ERentier <elinerentier@gmail.com>
2024-04-09 10:42:12 +0200

    create new paths for difference file. transpose diff file and flip x y visualisation

--- visualisation.r
+++ visualisation.r
@@ -3,20 +3,26 @@ install.packages("tidyverse")
 library("tidyverse")
  
 # Read the CSV file
-file_path <-("C:\\Users\\elren9761\\OneDrive - University of Bergen\\Documents\\PhD\\Analyses\\visualisation_dT\\delta_t_diff.csv")
-delta_t_diff <- read_delim(file_path,locale = locale(decimal_mark = ","))|>janitor::clean_names()
+file_path_dt <-("C:\\Users\\elren9761\\OneDrive - University of Bergen\\Documents\\PhD\\Analyses\\visualisation_dT\\Proxy_models_ggc_gmba_dem.csv")
+file_path_diff <- ("C:\\Users\\elren9761\\OneDrive - University of Bergen\\Documents\\PhD\\Analyses\\visualisation_dT\\dt_difference.csv")
+delta_t <- read_delim(file_path_dt,locale = locale(decimal_mark = ","))|>janitor::clean_names()
+delta_t_diff <- read_delim(file_path_diff,locale = locale(decimal_mark = ","))|>janitor::clean_names()
 
-delta_long <- delta_t_diff|>
-  dplyr::select(beyer_dt, chelsa_dt, ecoclimate_dt,paleopgem_dt,worldclim25m_dt,worldclim30s_dt,ggc_dt,gmted2010) |>
+#Transpose data for absolute values and difference values
+delta_t_long <- delta_t|>
+  dplyr::select(mean_dt, beyer_dt, chelsa_dt, ecoclimate_dt,paleopgem_dt,worldclim25m_dt,worldclim30s_dt,ggc_dt,gmted2010, in_mr) |>
   pivot_longer(cols = -gmted2010, 
                names_to = "Model", 
                values_to = "Values")
 
-# View the first few rows of the dataframe
-print(head(delta_t_diff))
+delta_diff_long <- delta_t_diff|>
+  dplyr::select(mean_dt, beyer_diff_mean, chelsa_diff_mean, ecoclimate_diff_mean,paleopgem_diff_mean,worldclim25m_diff_mean,worldclim30s_diff_mean,ggc_diff_mean,gmted2010, in_mr) |>
+  pivot_longer(cols = -gmted2010, 
+               names_to = "Model", 
+               values_to = "Values")
 
-
-ggplot(delta_long,aes(x=gmted2010, y=Values,color=Model))+
+# visualise the absolute values of dt against elevation
+ggplot(delta_diff_long,aes(x=Values, y=gmted2010,color=Model))+
   geom_point()+
   scale_y_continuous(breaks=c(0,min(delta_long$Values),
                               max(delta_long$Values)))+
