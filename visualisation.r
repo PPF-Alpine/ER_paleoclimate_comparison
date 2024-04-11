312efacc4ca4dbf3a216005685e20ff4c77f2691
ERentier <elinerentier@gmail.com>
2024-04-11 09:52:58 +0200

    update file paths to data folder and decimal to point when read the data in

--- visualisation.r
+++ visualisation.r
@@ -1,12 +1,18 @@
 # libraries
 install.packages("tidyverse")
 library("tidyverse")
+
+# library(conflicted)  
+# 
+# library(tidyverse)
+# conflict_prefer("filter", "dplyr")
+# conflict_prefer("lag", "dplyr")
  
 # Read the CSV file
-file_path_dt <-("C:\\Users\\elren9761\\OneDrive - University of Bergen\\Documents\\PhD\\Analyses\\visualisation_dT\\Proxy_models_ggc_gmba_dem.csv")
-file_path_diff <- ("C:\\Users\\elren9761\\OneDrive - University of Bergen\\Documents\\PhD\\Analyses\\visualisation_dT\\dt_difference.csv")
-delta_t <- read_delim(file_path_dt,locale = locale(decimal_mark = ","))|>janitor::clean_names()
-delta_t_diff <- read_delim(file_path_diff,locale = locale(decimal_mark = ","))|>janitor::clean_names()
+file_path_dt <-("data/Proxy_models_ggc_gmba_dem.csv")
+file_path_diff <- ("data/dt_difference.csv")
+delta_t <- read_delim(file_path_dt,locale = locale(decimal_mark = "."))|>janitor::clean_names()
+delta_t_diff <- read_delim(file_path_diff,locale = locale(decimal_mark = "."))|>janitor::clean_names()
 
 #Transpose data for absolute values and difference values
 delta_t_long <- delta_t|>
