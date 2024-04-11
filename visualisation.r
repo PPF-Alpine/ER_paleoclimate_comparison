ecac8a9405d1874c3a751315761f91047fdbe260
ERentier <elinerentier@gmail.com>
2024-04-11 12:14:29 +0200

    transpose tables, select within/outside mountain ranges and create boxplots of dt and dt diff

--- visualisation.r
+++ visualisation.r
@@ -8,32 +8,78 @@ library("tidyverse")
 # conflict_prefer("filter", "dplyr")
 # conflict_prefer("lag", "dplyr")
  
-# Read the CSV file
+# Read the CSV files
 file_path_dt <-("data/Proxy_models_ggc_gmba_dem.csv")
 file_path_diff <- ("data/dt_difference.csv")
 delta_t <- read_delim(file_path_dt,locale = locale(decimal_mark = "."))|>janitor::clean_names()
 delta_t_diff <- read_delim(file_path_diff,locale = locale(decimal_mark = "."))|>janitor::clean_names()
 
-#Transpose data for absolute values and difference values
+
+#Descriptive functions
+names(delta_t)
+names(delta_t_diff)
+typeof(delta_t$mean_dt)
+
+## Summary statistics
+# Filter the dataset to include only rows within mountain ranges, and one only outside mountain ranges
+delta_t_mountain <- delta_t %>%
+  filter(in_mr == 1)
+
+delta_t_lowlands <- delta_t %>%
+  filter(in_mr == 0)
+
+delta_t_diff_mountain <- delta_t_diff %>%
+  filter(in_mr == 1)
+
+delta_t_diff_lowlands <- delta_t_diff %>%
+  filter(in_mr == 0)
+
+#Transpose data
 delta_t_long <- delta_t|>
-  dplyr::select(mean_dt, beyer_dt, chelsa_dt, ecoclimate_dt,paleopgem_dt,worldclim25m_dt,worldclim30s_dt,ggc_dt,gmted2010, in_mr) |>
-  pivot_longer(cols = -gmted2010, 
-               names_to = "Model", 
+  dplyr::select(mean_dt, beyer_dt, chelsa_dt, ecoclimate_dt,paleopgem_dt,worldclim25m_dt,worldclim30s_dt,ggc_dt,gmted2010) |>
+  pivot_longer(cols = -gmted2010,
+               names_to = "Model",
+               values_to = "Values")
+
+delta_t_mr_long <- delta_t_mountain|>
+  dplyr::select(mean_dt, beyer_dt, chelsa_dt, ecoclimate_dt,paleopgem_dt,worldclim25m_dt,worldclim30s_dt,ggc_dt,gmted2010) |>
+  pivot_longer(cols = -gmted2010,
+               names_to = "Model",
                values_to = "Values")
 
 delta_diff_long <- delta_t_diff|>
-  dplyr::select(mean_dt, beyer_diff_mean, chelsa_diff_mean, ecoclimate_diff_mean,paleopgem_diff_mean,worldclim25m_diff_mean,worldclim30s_diff_mean,ggc_diff_mean,gmted2010, in_mr) |>
-  pivot_longer(cols = -gmted2010, 
-               names_to = "Model", 
+  dplyr::select(beyer_diff_mean, chelsa_diff_mean, ecoclimate_diff_mean,paleopgem_diff_mean,worldclim25m_diff_mean,worldclim30s_diff_mean,ggc_diff_mean,gmted2010) |>
+  pivot_longer(cols = -gmted2010,
+               names_to = "Model",
                values_to = "Values")
 
-# visualise the absolute values of dt against elevation
-# ggplot(delta_diff_long,aes(x=Values, y=gmted2010,color=Model))+
-#   geom_point()+
-#   scale_y_continuous(breaks=c(0,min(delta_long$Values),
-#                               max(delta_long$Values)))+
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+delta_t_diff_mr_long <- delta_t_diff_mountain|>
+  dplyr::select(beyer_diff_mean, chelsa_diff_mean, ecoclimate_diff_mean,paleopgem_diff_mean,worldclim25m_diff_mean,worldclim30s_diff_mean,ggc_diff_mean,gmted2010) |>
+  pivot_longer(cols = -gmted2010,
+               names_to = "Model",
+               values_to = "Values")
 
+## Plot boxplots for spread of each model
+# dT
+ggplot(delta_t_mr_long, aes(x = Model, y = Values, fill = Model)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("mean_dt" = "red", "other_models" = "blue")) +  # Specify colors for "mean_dt" and other models
+  labs(x = "Model", y = "ΔT") +
+  ggtitle("ΔT present-lgm for proxies within mountain ranges") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(delta_t_diff_mr_long, aes(x = Model, y = Values, fill = Model)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("other_models" = "blue")) + 
+  labs(x = "Model", y = "ΔT") +
+  ggtitle("ΔT diff (model-proxy value) within mountain ranges") +
+  geom_hline(yintercept = 0, color = "red", linetype = "solid") +  # Add horizontal red line at y = 0
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+## scatterplots
 # plot beyer mean and colour according to whether it is in or outside a mountain range
 ggplot(delta_t_diff, aes(x = beyer_diff_mean, y = gmted2010, color = factor(in_mr))) +
   geom_point(size = 3) +
