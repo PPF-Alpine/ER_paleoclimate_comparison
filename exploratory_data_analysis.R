b577f5f9c449f4a10c600e5709a150792fff9aa2
ERentier <elinerentier@gmail.com>
2024-04-15 13:10:21 +0200

    create new script for EDA

---
+++ exploratory_data_analysis.R
@@ -0,0 +1,18 @@
+#----------------------------------------------------------#
+#
+#           Paleoclimate comparison
+#
+#           exploratory data analysis
+#                 
+#
+#           Author: Eline Rentier 
+#                   2024
+#
+#----------------------------------------------------------#
+
+# purpose of the script
+
+#Descriptive functions
+names(delta_t) 
+names(delta_t_diff)
+typeof(delta_t$mean_dt)
