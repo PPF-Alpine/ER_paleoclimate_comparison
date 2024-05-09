f7b5b2d0f7ee24115aceeba7232f6f1cd4ad2e16
ERentier <elinerentier@gmail.com>
2024-05-09 17:53:46 +0200

    update list

--- required_packages.R
+++ required_packages.R
@@ -1,6 +1,15 @@
-install.packages("plotly")
-install.packages("tidyverse")
 
+# Install necessary packages if not already installed
+if (!require(readxl)) install.packages("readxl")
+if (!require(ggplot2)) install.packages("ggplot2")
+if (!require(plotly)) install.packages("plotly")
+if (!require(tidyverse)) install.packages("tidyverse")
+if (!require(dplyr)) install.packages("dplyr")
+
+# Load the packages
+library("readxl")
+library("ggplot2")
 library("tidyverse")
 library("plotly")
+library("dplyr")
 
